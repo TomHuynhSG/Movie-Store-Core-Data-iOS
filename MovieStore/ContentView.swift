169e5834e8cef9df8ca4019156c313a7c8d6304e
Tom Huynh <tomhuynhsg@gmail.com>
2022-09-04 14:24:16 +0700

    Initial Commit

---
+++ MovieStore/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MovieStore
+//
+//  Created by Tom Huynh on 9/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MovieStore/MovieStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieStoreApp.swift
+//  MovieStore
+//
+//  Created by Tom Huynh on 9/4/22.
+//
+
+import SwiftUI
+
+@main
+struct MovieStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
