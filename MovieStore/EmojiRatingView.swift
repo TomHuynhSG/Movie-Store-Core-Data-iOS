84b662f316582a7c94d8f00254ca870a8c9f6ef7
Tom Huynh <tomhuynhsg@gmail.com>
2022-09-05 00:22:02 +0700

    Change the emoji to be more expressive

--- MovieStore/EmojiRatingView.swift
+++ MovieStore/EmojiRatingView.swift
@@ -12,9 +12,9 @@ struct EmojiRatingView: View {
     var body: some View {
         switch rating {
         case 1:
-            return Text("🥲")
+            return Text("🫣")
         case 2:
-            return Text("😒")
+            return Text("😕")
         case 3:
             return Text("🙂")
         case 4:
