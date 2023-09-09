a11eb66091363dec2d327c039ea54d08ac33b1a4
Tom Huynh <tomhuynhsg@gmail.com>
2023-09-09 21:02:03 +0700

    Clean up the code

--- MovieStore/DataController.swift
+++ MovieStore/DataController.swift
@@ -12,11 +12,8 @@ struct DataController {
 
     let container: NSPersistentContainer
 
-    init(inMemory: Bool = false) {
+    init() {
         container = NSPersistentContainer(name: "MovieDataModel")
-        if inMemory {
-            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
-        }
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 fatalError("Unresolved error \(error), \(error.userInfo)")
